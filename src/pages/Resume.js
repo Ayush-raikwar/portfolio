2d4e4e14433fce53d0e24f58ca65fce94f4789af
Ayush-raikwar <ayushraikwar45@gmail.com>
2025-01-14 17:28:24 +0530

    added download btn with functionality

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -13,17 +13,24 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 
 export const Resume = () => {
 
+    function downloadPDF() {
+        const pdfUrl = "https://drive.google.com/file/d/12IKPcyW5kpVM9KKQjULrtZ_hjfb_YIBF/view?usp=sharing";
+        const link = document.createElement("a");
+        link.href = pdfUrl;
+        link.setAttribute("download", "sample.pdf");
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
     return (
         <Container>
             <Section>
-                <PdfContainer
-                    file={pdfFile}>
-                    <Page
-                        renderTextLayer={false}
-                        renderAnnotationLayer={false}
-                        pageNumber={1}
-                    />
-                </PdfContainer>
+                <ResumeImg
+                    src='https://previews.dropbox.com/p/thumb/AChyupdugkydCqXjNuSoxM2EPoU2wwNdT8rgiPWihnjZdDoYVgTEtRSKypqs3R7CyzT3uEi99rgsqyNfmEFoAVpmN91mtPfWfcQ__ujsU1gnWcAWHGhIhD5vVls6nLlTQLElRQ2nYFW-3I2ZRBHafhP2uqndLGevCqmO2DyNULDx0mGw6KJAnMmDQS0h3-jMtiEB6sN0ObGtkQMS1FJKuizemQuCJ0UjHA8xFMGKZMvaVOjPF3MLT2dtjXRXRl7-W-V86QUUjENcE3g9doGVQRNJg25aGoe1gswLKsIE79vrHWYR8rIM4oJ9aBfyBdSsnzuD-giWMfuEGsnpnPncpYmq/p.jpeg?is_prewarmed=true'
+                    alt='resume-img'
+                />
+                <button onClick={downloadPDF}>Download</button>
             </Section>
             <Footer />
         </Container>
@@ -46,18 +53,19 @@ const Section = styled.section`
     align-items: center;
     justify-content: center;
     width: auto;
+
+    button {
+        position: absolute;
+        border: 1px solid grey;
+        border-radius: 0.5rem;
+        padding: 0.5rem;
+        background-color: ${styles.colors.white};
+        cursor: pointer;
+        top: 11%;
+        text-transform: uppercase;
+    }
 `
 
-const PdfContainer = styled(Document)`
-    margin: auto;
-    div{
-        width: 95%;    
-        margin: auto !important;
-        @media (max-width: 600px) {
-            canvas{
-                width: 100% !important;
-                height: 80% !important;
-            }
-        }
-    }
+const ResumeImg = styled.img`
+    width: 500px;
 `
